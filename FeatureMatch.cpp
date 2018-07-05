3ce52b07620f446445f4db26ca853716bc357298
dale <2911577624@qq.com>
2018-07-05 14:47:18 +0800

    Add files via upload

---
+++ FeatureMatch.cpp
@@ -0,0 +1,149 @@
+#include "stdafx.h"
+#include "FeatureMatch.h"
+
+
+void FeatureMatch::FeatureObtatinAndMatchForAll(vector<Mat>& images,
+	vector<vector<KeyPoint>>& key_points_for_all,
+	vector<Mat>& descriptor_for_all,
+	vector<vector<Vec3b>>& colors_for_all,
+	vector<vector<DMatch>>& matches_for_all) 
+{
+	OrbFeatureMatch(images, key_points_for_all, descriptor_for_all, colors_for_all);
+	matchFeatures(//image_for_all, 
+		descriptor_for_all,
+		matches_for_all);
+}
+
+void FeatureMatch::OrbFeatureMatch(vector<Mat>& images,
+	vector<vector<KeyPoint>>& key_points_for_all,
+	vector<Mat>& descriptor_for_all,
+	vector<vector<Vec3b>>& colors_for_all
+)
+{
+	key_points_for_all.clear();
+	descriptor_for_all.clear();
+
+	Ptr<ORB> orb = ORB::create();//(500, 1.1f, 10,  31, 0,  2, ORB::HARRIS_SCORE, 31);orb参数设置研究中
+
+	Mat image;
+	for (auto it = images.begin(); it != images.end(); ++it)
+	{
+		image = *it;
+		if (image.empty()) continue;
+
+		cout << "Extracing features: " << *it << endl;
+
+		vector<KeyPoint> key_points;
+		Mat descriptor;
+
+		//偶尔出现内存分配失败的错误
+		orb->detectAndCompute(image, Mat(), key_points, descriptor);
+
+		//特征点过少，则排除该图像
+		if (key_points.size() <= 10) continue;
+
+		key_points_for_all.push_back(key_points);
+		descriptor_for_all.push_back(descriptor);
+
+		vector<Vec3b> colors(key_points.size());
+
+		for (int i = 0; i < key_points.size(); ++i)
+		{
+			Point2f& p = key_points[i].pt;
+			colors[i] = image.at<Vec3b>(p.y, p.x);
+		}
+		colors_for_all.push_back(colors);
+	}
+}
+
+void 
+FeatureMatch::matchFeatures
+(	Mat & query,
+	Mat & train,
+//	Mat& descriptors1,
+//	Mat& descriptors2,
+	vector<DMatch>& matches)
+{
+	//Ptr<FeatureDetector> detector = ORB::create();
+	//Ptr<DescriptorExtractor> descriptor = ORB::create();
+	//Ptr<DescriptorMatcher> matcher_1 = DescriptorMatcher::create("BruteForce-Hamming");
+	////检测 Oriented FAST 角点位置
+	//vector<KeyPoint> l_keyPoints;
+	//vector<KeyPoint> r_keyPoints;
+	//detector->detect(query, l_keyPoints);
+	//detector->detect(train, r_keyPoints);
+	////根据角点位置计算 BRIEF 描述子
+	//descriptor->compute(query, l_keyPoints, descriptors1);
+	//descriptor->compute(query, r_keyPoints, descriptors2);
+	////对两幅图像中的BRIEF描述子进行匹配，使用 Hamming 距离
+	//vector<DMatch> matches_1;
+	////BFMatcher matcher ( NORM_HAMMING );
+	//matcher_1->match(descriptors1, descriptors2, matches_1);
+	////匹配点对筛选
+	//double min_dist = 1000, max_dist = 10;
+	////找出所有匹配之间的最小距离和最大距离, 即是最相似的和最不相似的两组点之间的距离
+	//for (int i = 0; i < descriptors1.rows; i++)
+	//{
+	//	double dist = matches_1[i].distance;
+	//	if (dist < min_dist) min_dist = dist;
+	//	if (dist > max_dist) max_dist = dist;
+	//}
+	////筛选
+	//for (int i = 0; i < descriptors1.rows; i++)
+	//{
+	//	if (matches_1[i].distance <= max(2 * min_dist, 20.0))
+	//	{
+	//		matches.push_back(matches_1[i]);
+	//	}
+	//}
+	vector<vector<DMatch>> knn_matches;
+	BFMatcher matcher(NORM_L2);
+	matcher.knnMatch(query, train, knn_matches, 2);
+
+	//获取满足Ratio Test的最小匹配的距离
+	float min_dist = FLT_MAX;
+	for (int r = 0; r < knn_matches.size(); ++r)
+	{
+		//Ratio Test
+		if (knn_matches[r][0].distance > 0.6*knn_matches[r][1].distance)
+			continue;
+
+		float dist = knn_matches[r][0].distance;
+		if (dist < min_dist) min_dist = dist;
+	}
+
+	matches.clear();
+	for (size_t r = 0; r < knn_matches.size(); ++r)
+	{
+		//排除不满足Ratio Test的点和匹配距离过大的点
+		if (
+			knn_matches[r][0].distance > 0.6*knn_matches[r][1].distance ||
+			knn_matches[r][0].distance > 5 * max(min_dist, 10.0f)
+			)
+			continue;
+		//保存匹配点
+		matches.push_back(knn_matches[r][0]);
+	}
+}
+
+void 
+FeatureMatch::matchFeatures
+(
+	vector<Mat>& descriptor_for_all, 
+	vector<vector<DMatch>>& matches_for_all
+) 
+{
+	matches_for_all.clear();
+	// n个图像，两两顺次有 n-1 对匹配
+	for (int i = 0; i < descriptor_for_all.size() - 1; ++i)
+	{
+		cout << "Matching images " << i << " - " << i + 1 << endl;
+
+		vector<DMatch> matches;
+
+		matchFeatures(descriptor_for_all[i], descriptor_for_all[i + 1], matches);
+		
+		//储存n-1个matches结果
+		matches_for_all.push_back(matches);
+	}
+}
---
+++ Structure.cpp
@@ -0,0 +1,63 @@
+#include "stdafx.h"
+#include "Structure.h"
+
+void Structure::init_structure(
+	vector<vector<KeyPoint>>& key_points_for_all,
+	vector<vector<Vec3b>>& colors_for_all,
+	vector<vector<DMatch>>& matches_for_all,
+
+	vector<Point3f>& structure,//保存所有相机的齐次坐标
+	vector<vector<int>>& correspond_struct_idx,//2幅图像的结构索引
+	vector<Vec3b>& colors,
+	vector<Mat>& rotations,
+	vector<Mat>& motions
+)
+{
+	//计算头两幅图像之间的变换矩阵
+	vector<Point2f> p1, p2;
+	vector<Vec3b> c2;//无作用
+	Mat R, T;	//旋转矩阵和平移向量
+
+	Mat mask;	//mask中大于零的点代表匹配点，等于零代表失配点
+	Transform::get_matched_points(key_points_for_all[0], key_points_for_all[1], matches_for_all[0], p1, p2);
+	Transform::get_matched_colors(colors_for_all[0], colors_for_all[1], matches_for_all[0], colors, c2);
+
+	Transform::find_transform(p1, p2, R, T, mask);
+
+	//对头两幅图像进行三维重建
+	//排除误匹配的点儿
+	Mask::maskout_points(p1, mask);
+	Mask::maskout_points(p2, mask);
+	Mask::maskout_colors(colors, mask);
+
+	//第一个相机设置为世界坐标
+	Mat R0 = Mat::eye(3, 3, CV_64FC1);//矩阵规范化
+	Mat T0 = Mat::zeros(3, 1, CV_64FC1);
+
+	Reconstruct::reconstruct(R0, T0, R, T, p1, p2, structure);
+
+	//保存每一个相机相对于前一个相机的变换矩阵//初始化加入2个 n-1+1
+	rotations = { R0, R };
+	motions = { T0, T };
+
+	//将correspond_struct_idx的大小初始化为与key_points_for_all完全一致
+	correspond_struct_idx.clear();
+	correspond_struct_idx.resize(key_points_for_all.size());//n个相机n个结构索引
+	for (int i = 0; i < key_points_for_all.size(); ++i)//
+	{
+		correspond_struct_idx[i].resize(key_points_for_all[i].size(), -1);//
+	}
+
+	//填写头两幅图像的结构索引
+	int idx = 0;
+	vector<DMatch>& matches = matches_for_all[0];
+	for (int i = 0; i < matches.size(); ++i)
+	{
+		if (mask.at<uchar>(i) == 0)
+			continue;
+		//matches[i].queryIdx为keyPoints中matches点的坐标
+		correspond_struct_idx[0][matches[i].queryIdx] = idx;
+		correspond_struct_idx[1][matches[i].trainIdx] = idx;
+		++idx;
+	}
+}
